3164aa16ca470aac6c2b147abd686ac5d5c975db
macbeamish <75453707+macbeamish@users.noreply.github.com>
2022-04-10 03:59:19 +1200

    fixes
    fixed up some bad code, may still need work

--- js/site.js
+++ js/site.js
@@ -19,20 +19,43 @@ function getValues(){
             //calculates the remaining balance
             if (i==0) {
                 //if first month
-                monthFiguresObj.remainBal=loanValue;          
+                monthFiguresObj.remainBal=loanValue;    
+                monthFiguresObj.monthlyInt=Math.round((calcMonthlyInt(loanValue,rateValue))*100)/100;     
             }
             else{
-                //all following months
-                monthFiguresObj.remainBal= Math.round((calcRemainingBal(loanValue,i,(arrayMonths[(i-1)].monthlyPri)))*100)/100;
+                 //calls function to calculate the monthly interest payment stores value in return object
+                monthFiguresObj.monthlyInt = Math.round((calcMonthlyInt(arrayMonths[i-1].remainBal,rateValue))*100)/100;
             }
-            //calls function to calculate the monthly interest payment stores value in return object
-            monthFiguresObj.monthlyInt = Math.round((calcMonthlyInt(monthFiguresObj.remainBal,rateValue))*100)/100;
-
+           
             //calls function to calculate monthly principal
             monthFiguresObj.monthlyPri = Math.round((calcMonthlyPri(monthFiguresObj.tMonthlyPay,monthFiguresObj.monthlyInt))*100)/100;
 
-            monthFiguresObj.intPaidToDate = Math.round((monthFiguresObj.monthlyInt*i)*100)/100;
-           
+            //stores the index as the month, may not be neccessary but adding for redundancy
+            monthFiguresObj.month=i;
+            
+            let runningIntTotal = 0;
+            //calculates interest paid to date
+            for (let i = 0; i < arrayMonths.length; i++) {
+               
+                 runningIntTotal += arrayMonths[i].monthlyInt;
+            }
+          
+            monthFiguresObj.intPaidToDate = Math.round((runningIntTotal)*100)/100;
+
+            let runningPriTotal = 0;
+
+            //calculates interest paid to date
+            for (let i = 0; i < arrayMonths.length; i++) {
+            
+                runningPriTotal += arrayMonths[i].monthlyPri;
+            }
+            
+            monthFiguresObj.priPaidToDate = Math.round((runningPriTotal)*100)/100;
+
+             //all following months
+             monthFiguresObj.remainBal= Math.round((calcRemainingBal(loanValue,monthFiguresObj.priPaidToDate))*100)/100;
+
+
             //pushes object to array
             arrayMonths.push(monthFiguresObj);
             
@@ -82,12 +105,29 @@ function calcMonthlyPri(tMonthlyPay,intPay){
  }
 
 //get remaining balance
-function calcRemainingBal(loan,month,monthlyPrin){
-let remaingBal = loan - (month*monthlyPrin);
-return remaingBal;
+function calcRemainingBal(loan,priPaidToDate){
+ let remainBal = loan - priPaidToDate;
+if(remainBal>0)
+return remainBal;
+else{
+    remainBal=0
+    return remainBal;
+}
+}
+//calculates interest paid to date
+function calcIntPaid(arrayMonths){
+let intPaid = 0;
+for (let i = 0; i < array.length; index++) {
+    const element = array[index];
+    
+}
 }
 
+//calculates interest paid to date
+function calcPriPaid(arrayMonths){
+let PriPaid =  0;  
 
+}
 
 // display or view functions
 function displayResults(arrayMonths){
@@ -95,10 +135,10 @@ function displayResults(arrayMonths){
     let templateRows = "`<tr><th>Month</th><th>Total Monthly Payment</th><th>Principal Payment</th><th>Interest Payment</th><th>Interest Paid to Date</th><th>Remaining Loan Balance</th></tr>`";
     
     document.getElementById("results").innerHTML = templateRows
-    for (let i = 1; i <= arrayMonths.length; i++) {
+    for (let i = 0; i <= arrayMonths.length; i++) {
         
         
-        templateRows +=`<tr><td>${i}</td><td>$${arrayMonths[i].tMonthlyPay}</td><td>$${arrayMonths[i].monthlyPri}</td><td>$${arrayMonths[i].monthlyInt}</td><td>$${arrayMonths[i].intPaidToDate}</td><td>$${arrayMonths[i].remainBal}</td></tr>`;
+        templateRows +=`<tr><td>${arrayMonths[i].month}</td><td>$${arrayMonths[i].tMonthlyPay}</td><td>$${arrayMonths[i].monthlyPri}</td><td>$${arrayMonths[i].monthlyInt}</td><td>$${arrayMonths[i].intPaidToDate}</td><td>$${arrayMonths[i].remainBal}</td></tr>`;
         document.getElementById("results").innerHTML = templateRows;
 
     }
